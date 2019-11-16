7534b714d7b42697101f26518c48c271f8d4b3cc
sushuqing <sushuqing@weiyankeji.cn>
2019-11-16 20:47:18 +0800

    change the company layouts

--- src/components/product_industry/product_industry.vue
+++ src/components/product_industry/product_industry.vue
@@ -1,12 +1,40 @@
 <template>
     <div class="product_industry">
         <comTitle :title="comTitle" :linkInfo="linkInfo"></comTitle>
-        <el-link v-for="(factory,index) in manufacture_list" :underline="false" :href="factory.url_address" target="_blank" type="primary">{{factory.company_name}}</el-link>
+        <div class="product_industry_content">
+            <el-col v-for="(factory,index) in manufacture_list.general" :span="4">
+                <el-link class="imgscale-top" :underline="false" :href="factory.url_address" target="_blank" type="primary">
+                    <img :src="factory.icon_address" width="16" height="16" class="nav-icon"></img>
+                    {{factory.company_name}}
+                </el-link>
+            </el-col>
+            <el-col class = "vip" v-for="(factory,index) in manufacture_list.vip" :span="4">
+                <div class="vip_content imgscale-top boxshodow-hover">
+                    <a :underline="false" :href="factory.url_address" target="_blank" type="primary">
+                        <div class="img_box">
+                            <div class="imgscale">
+                                <img :src="factory.vip_icon_address" :alt="factory.company_name">
+                            </div>
+                        </div>
+                        <div class="title_box">
+                            <div class="title ellipsis-2">
+                                {{factory.company_name}}
+                            </div>
+                        </div>
+                        <div class="com_tel">
+                            <div class="ellipsis-2">
+                                发财热线: {{factory.com_tel}}
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </el-col>
+        </div>
     </div>
 </template>
 <script>
     import Vue from 'vue';
-    import { Divider } from 'element-ui';
+    import { Divider, Row, Col, } from 'element-ui'
     import 'element-ui/lib/theme-chalk/index.css';
     import comTitle from '~/components/common/comTitle.vue'
     import { getProducIndustry } from '~/api/index.js'
@@ -46,12 +74,23 @@
         },
         data(){
             return{
-                manufacture_list:[
-                    {
-                        "company_name":"XXX厂创",
-                        "url_address":"http://www.baidu.com"
-                    }
-                ],
+                manufacture_list:{
+                    "general":[
+                        {
+                            "company_name":"XXX厂创",
+                            "url_address":"http://www.baidu.com",
+                            "icon_address":"https://ss2.bdstatic.com/kfoZeXSm1A5BphGlnYG/icon/95490.png"
+                        }
+                    ],
+                    "vip":[
+                        {
+                            "company_name":"XXX厂创",
+                            "url_address":"http://www.baidu.com",
+                            "vip_icon_address":"https://b3.hoopchina.com.cn/images/logo2017/v1/hp_logo_sports.png",
+                            "com_tel":"18818262629"
+                        }
+                    ]
+                },
                 com_title:""
             };
         }
@@ -75,6 +114,76 @@
         padding: 2px 5px;
         font-size:16px;
     }
+    .product_industry_content{
+        margin:20px 20px;
+    }
+    .nav-icon{
+        margin-left:15px;
+        margin-top:12px;
+    }
+    .vip{
+        margin: 0;
+        padding: 0 0 16px 16px;
+        height: 74px;
+    }
+    .vip_content{
+        position: relative;
+        border-radius: 8px;
+        height:100%;
+        box-shadow: 0 1px 4px 0 rgba(0,0,0,.1);        
+    }
+    .vip a{
+        display:block;
+        text-decoration:none;
+        color:#409EFF;
+        padding-left:50%;
+        width:50%;
+        height:100%;
+        font-size:8px;
+    }
+    .product_industry_content a:{
+        color:black;
+    }
+    .product_industry_content a:hover{
+        background-color:#FFFFFF;
+    }
+    .img_box {
+        width: 40%;
+        position: absolute;
+        left: 0;
+        top: 0;
+        padding: 10px;
+    }
+    .title_box{
+        padding-top:10px;
+    }
+    .imgscale {
+        overflow: hidden;
+        border-radius: 8px!important;
+    }
+    .imgscale img {
+        transition: all .4s ease-in-out;
+        width: 100%;
+    }
+    .imgscale img:hover {
+        transform: scale(1.1);
+    }
+    .ellipsis-2 {
+        display: -webkit-box!important;
+        overflow: hidden;
+        white-space: normal!important;
+        text-overflow: ellipsis;
+        word-wrap: break-word;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+    }
+    .boxshodow-hover:hover {
+        box-shadow: 0 2px 8px 0 rgba(0,0,0,.1)!important
+    }
+    .imgscale-top:hover img {
+        transform: scale(1.1)
+    }
+
 </style>
 
 
